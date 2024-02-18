ac5fcd3f5b960c5c019d4b1ce4d43211008d54e0
Daniel G <nwrkbiz@users.noreply.github.com>
2024-02-18 14:06:58 +0000

    fix: honor ssh port

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,9 +2,12 @@
 
 if [ "$GIT_URL" != "${GIT_URL#ssh://}" ] ; then
   eval $(ssh-agent)
-  DOMAIN=echo "$GIT_URL" | awk -F@ '{print $2}' | awk -F/ '{print $1}'
-  ssh-keyscan $DOMAIN >> ~/.ssh/known_hosts
-  git clone $GIT_URL $WORKSPACE_FOLDER
+  DOMAIN=$(echo "$GIT_URL" | awk -F@ '{print $2}' | awk -F/ '{print $1}' | awk -F: '{print $1}')
+  PORT=$(echo "$GIT_URL" | awk -F: '{print $3}' | awk -F/ '{print $1}')
+  PORT=${PORT:-22}
+  mkdir -p  ~/.ssh
+  ssh-keyscan -p $PORT $DOMAIN >> ~/.ssh/known_hosts
+  git clone $GIT_URL $WORKSPACE_FOLDER --recurse-submodules
 fi
 
 /.envbuilder/bin/envbuilder
