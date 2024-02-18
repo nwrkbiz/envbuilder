951a48340c85ea375c98590a4747f5514a13bfa4
Daniel G <nwrkbiz@users.noreply.github.com>
2024-02-18 13:09:18 +0000

    fix: accept ssh fingerprint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if [ "$GIT_URL" != "${GIT_URL#ssh://}" ] ; then
+  eval $(ssh-agent)
+  DOMAIN=echo "$GIT_URL" | awk -F@ '{print $2}' | awk -F/ '{print $1}'
+  ssh-keyscan $DOMAIN >> ~/.ssh/known_hosts
   git clone $GIT_URL $WORKSPACE_FOLDER
 fi
-
 
 /.envbuilder/bin/envbuilder
