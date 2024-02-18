8279b07f49f9c865019d877261c6dd85a787e534
Daniel G <nwrkbiz@users.noreply.github.com>
2024-02-18 22:28:37 +0000

    fix: repair awk syntax

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+set -x
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
 /tmp/code-server/bin/code-server --extensions-dir /tmp/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
 
 if [ "${GIT_URL}" != "${GIT_URL#ssh://}" ] ; then
-    DOMAIN=$(echo "${GIT_URL}" | awk -F@ "{print $2}" | awk -F/ "{print $1}" | awk -F: "{print $1}")
-    PORT=$(echo "${GIT_URL}" | awk -F: "{print $3}" | awk -F/ "{print $1}")
+    DOMAIN=$(echo "${GIT_URL}" | awk -F@ '{print $2}' | awk -F/ '{print $1}' | awk -F: '{print $1}')
+    PORT=$(echo "${GIT_URL}" | awk -F: '{print $3}' | awk -F/ '{print $1}')
     PORT=${PORT:-22}
     mkdir -p  ~/.ssh
     ssh-keyscan -p ${PORT} ${DOMAIN} >> ~/.ssh/known_hosts
