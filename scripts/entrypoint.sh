00d63119958b18fd0939ed6c14e3551e09112384
Daniel G <nwrkbiz@users.noreply.github.com>
2024-02-27 19:54:02 +0000

    adjust script for debugging

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,7 @@ set -x
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
 /tmp/code-server/bin/code-server --extensions-dir /tmp/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
 
+echo "${GIT_URL}" > ~/giturl
 if [ "${GIT_URL}" != "${GIT_URL#ssh://}" ] ; then
     DOMAIN=$(echo "${GIT_URL}" | awk -F@ '{print $2}' | awk -F/ '{print $1}' | awk -F: '{print $1}')
     PORT=$(echo "${GIT_URL}" | awk -F: '{print $3}' | awk -F/ '{print $1}')
@@ -12,6 +13,6 @@ if [ "${GIT_URL}" != "${GIT_URL#ssh://}" ] ; then
     git clone ${GIT_URL} ${WORKSPACE_FOLDER} --recurse-submodules
 fi
 
-killall coder || :
-killall vscode || :
-/.envbuilder/bin/envbuilder
+#killall coder || :
+#killall vscode || :
+#/.envbuilder/bin/envbuilder
